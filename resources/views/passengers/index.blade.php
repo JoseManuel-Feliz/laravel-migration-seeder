de1ba0b9e0369434316038d24ca713606b7fdff7
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-10-28 16:48:24 +0100

    added trains seeder

--- resources/views/passengers/index.blade.php
+++ resources/views/passengers/index.blade.php
@@ -1,3 +1,15 @@
 @extends('layouts.app')
 
 @section('page-title', 'passengers-data')
+
+@section('main-content')
+
+<main>
+
+    @foreach($passengers as $passenger)
+    @dd($passenger)
+    @endforeach
+
+</main>
+
+@endsection
