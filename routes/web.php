894b6a66b725576b98b80573321c5924fa2edc0c
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-28 12:27:10 +0100

    added passenger route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TrainController;
+use App\Http\Controllers\PassengerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/trains', [TrainController::class, 'index']);
+Route::get('/passengers', [PassengerController::class, 'index']);
